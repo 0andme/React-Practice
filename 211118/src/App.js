e3b7494a4c7933f78ac7e9b8b5a5f255ac63ebad
0andme <yu00mi97@gmail.com>
2021-11-23 01:54:44 +0900

    feat : 211118 프로젝트 생성

---
+++ 211118/src/App.js
@@ -0,0 +1,8 @@
+import logo from "./logo.svg";
+import "./App.css";
+import C1_Sample from "./component/C1_Sample";
+function App() {
+  return <C1_Sample />;
+}
+
+export default App;
---
+++ 211118/src/component/C1_Sample.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+
+function C1_Sample() {
+  return <div>C1_Sample</div>;
+}
+export default C1_Sample;
