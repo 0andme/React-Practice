2b3d31417dc06409957468a0b837654254b60d39
0andme <yu00mi97@gmail.com>
2021-11-23 20:07:06 +0900

    feat : app.js에 컴포넌트 연결

--- 211118/src/App.js
+++ 211118/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
 import C1_Sample from "./component/C1_Sample";
+import C2_AddClassName from "./component/C2_AddClassName";
+import C3_PropsData from "./component/C3_PropsData";
+import C4_PropsDataType from "./component/C4_PropsDataType";
+import C5_PropsDataTypes from "./component/C5_PropsDataTypes";
 function App() {
-  return <C1_Sample />;
+  return (
+    <div>
+      <C1_Sample />
+      <C2_AddClassName />
+      <C3_PropsData name="부모로부터 받은 string" />
+      <C4_PropsDataType name={"String데이터만 보내기"} />
+      {/* <C4_PropsDataType name={false} /> */}
+      <C5_PropsDataTypes
+        boolValue={true}
+        numValue={112}
+        arrayValue={[1, 2, 3]}
+        objValue={{ name: "강강강", age: 30 }}
+        nodeValue={<h1>헤더</h1>}
+        funcValue={console.log("하하하")}
+      />
+    </div>
+  );
 }
-
 export default App;
