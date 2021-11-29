f86601befdd0731e7f77f67f8f17bfd45aa98211
0andme <yu00mi97@gmail.com>
2021-11-29 20:29:35 +0900

    feat : 컴포넌트 정의하는 세가지

---
+++ online/src/component/컴포넌트정의.js
@@ -0,0 +1,24 @@
+// 컴포넌트 정의
+import React, { Component } from "react";
+
+// 1번 클래스
+class componentName1 extends Component {
+  render() {
+  
+    return <div></div>;
+  }
+}
+
+export default componentName1;
+
+// 2번 함수
+function componentName2(){
+  return <div></div>;
+}
+export default componentName2;
+// 3번 화살표 함수
+
+const componentName =()=>{
+  return <div></div>
+}
+export default componentName3;
