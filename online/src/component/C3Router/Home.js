59f249e480b0f05c5a5f5c7a7b5158ebab8bef7a
0andme <yu00mi97@gmail.com>
2021-12-02 22:46:19 +0900

    feat : js로 버튼 눌러 라우터 이동 useNavigate

--- online/src/component/C3Router/Home.js
+++ online/src/component/C3Router/Home.js
@@ -1,14 +1,18 @@
 import React from "react";
 // import { useNavigate, useLocation, useParams } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 function Home(props) {
   let prams = useParams();
   console.log(prams.id, typeof prams.id);
-
+  const his = useNavigate();
+  function gotoAbout() {
+    his("/about");
+  }
   return (
     <div>
       <span>Home</span>
+      <button onClick={gotoAbout}>About으로 가는 버튼</button>
       {prams.id && <p>{prams.id}</p>}
     </div>
   );
