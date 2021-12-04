4aa0af008b583c05895926750c283ad74b1fb229
0andme <yu00mi97@gmail.com>
2021-12-05 08:02:59 +0900

    feat : about useState

--- react-hook/src/App.js
+++ react-hook/src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Example from "./components/Example";
 import Example2 from "./components/Example2";
+import Example3 from "./components/Example3";
 
 function App() {
   return (
     <div className="App">
       <Example />
       <Example2 />
+      <Example3 />
     </div>
   );
 }
