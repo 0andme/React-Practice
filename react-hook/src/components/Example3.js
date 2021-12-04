af37238a200d7dfa0d8a6e5dbfe84dbab4924ddf
0andme <yu00mi97@gmail.com>
2021-12-05 08:00:50 +0900

    feat : about useState

--- react-hook/src/components/Example3.js
+++ react-hook/src/components/Example3.js
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 
 function Example3() {
-  const [count, setCount] = useState(0);
+  const [state, setState] = useState({ count: 0 });
   return (
     <div>
-      <p>click cnt : {count}</p>
+      <p>click cnt : {state.count}</p>
       <button onClick={click}>UP</button>
     </div>
   );
   function click() {
-    setCount(count + 1);
+    // setState((state)=>count + 1);
+    setState((state) => {
+      return { count: state.count + 1 };
+    });
   }
 }
 export default Example3;
